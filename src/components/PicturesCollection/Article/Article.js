1c199f80c7264ac7e33eeb81cf2b31dce66175ae
Klaudia <kklaudiaw@gmail.com>
2021-01-28 15:22:49 +0100

    fetching images data from json file

--- src/components/PicturesCollection/Article/Article.js
+++ src/components/PicturesCollection/Article/Article.js
@@ -1,41 +1,45 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
 import './Article.css';
+import Auxiliary from '../../../hoc/Auxiliary';
 import heart from '../../../assets/svg/heart-solid.svg';
 
 const Article = () => {
+  const [data, setData] = useState([]);
 
-    useEffect(()=>{
-        fetch('data.json', {
-            headers : { 
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'
-             }
-          })
-        .then(response => response.json())
-        .then(responseData => {
-            console.log(responseData)
-        })
-            // const loadedImages = [];
-            // for (const key in responseData){
-            //     loadedImages.push({
-            //         id: key
-            //         // name: responseData[key].Name,
-            //         // image: responseData[key].Image,
-            //         // date:  responseData[key].Date,
-            //         // place:  responseData[key].Place,
-            //         // tags: responseData[key].Tags
-            //     })
-            },[])
-        
-return (
-<article className="Article">
-<img className="Picture" src="https://cdn.pixabay.com/photo/2021/01/10/12/00/road-5904909_1280.jpg" alt="road by the sea"/>
-<p className="P">Data: 19.01.2021</p>
-<p className="P">Miejsce wykonania: XYZ</p>
-<p className="P">Tagi: #zachódsłońca #krajobraz</p>
-<img src={heart} className="Heart"  alt="heart"/>
-</article>
-
-)
+  const getData = () => {
+    fetch('data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        setData(responseData);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  return (
+    <Auxiliary>
+      {data &&
+        data.length > 0 &&
+        data.map((item) => {
+          return (
+            <article className="Article">
+              <img className="Picture" src={item.Image} alt={item.Name} />
+              <p>{item.Date}</p>
+              <p className="P">Miejsce wykonania: {item.Place}</p>
+              <p className="P">Data: {item.Date}</p>
+              <p className="P">Tagi: {item.Tags}</p>
+              <img src={heart} className="Heart" alt="heart" />
+            </article>
+          );
+        })}
+    </Auxiliary>
+  );
 };
+
 export default Article;
