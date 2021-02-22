919e5b3c76e0a160bc32e312c024103c0e81d636
Klaudia <kklaudiaw@gmail.com>
2021-02-22 23:13:41 +0100

    context

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import styles from './NewPost.module.css';
+import DataContext from '../../context/DataContext';
 
 const NewPost = () => {
+  const picturesData = useContext(DataContext);
   const [inputState, setInputState] = useState({
     id: new Date().getTime(),
     name: '',
@@ -16,9 +18,9 @@ const NewPost = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    console.log(inputState);
+    picturesData.push(inputState);
+    console.log(inputState, picturesData);
   };
-
   const newValueHandler = (event) => {
     const newValue = event.target.value;
     setInputState((prevInputState) => ({
@@ -27,86 +29,86 @@ const NewPost = () => {
     }));
   };
   return (
-    <form className={styles.Form} onSubmit={submitHandler}>
-      <button className={styles.Button} type="submit">
-        Zapisz
-      </button>
+        <form className={styles.Form} onSubmit={submitHandler}>
+          <button className={styles.Button} type="submit">
+            Zapisz
+          </button>
 
-      <Link to="/">
-        <button className={styles.Button}>Anuluj</button>
-      </Link>
-      <div className={styles.Option}>
-        <label className={styles.Label} htmlFor="name">
-          Nazwa
-        </label>
-        <input
-          className={styles.Input}
-          type="text"
-          name="name"
-          id="name"
-          value={inputState.name}
-          onChange={newValueHandler}
-          required
-        ></input>
-      </div>
-      <div className={styles.Option}>
-        <label className={styles.Label} htmlFor="url">
-          URL
-        </label>
-        <input
-          className={styles.Input}
-          type="text"
-          name="url"
-          id="url"
-          onChange={newValueHandler}
-          required
-          value={inputState.url}
-        ></input>
-      </div>
-      <div className={styles.Option}>
-        <label className={styles.Label} htmlFor="date">
-          Data
-        </label>
-        <input
-          className={styles.Input}
-          type="date"
-          name="date"
-          id="date"
-          placeholder=""
-          required
-          onChange={newValueHandler}
-          value={inputState.date}
-        ></input>
-      </div>
-      <div className={styles.Option}>
-        <label className={styles.Label} htmlFor="place">
-          Miejsce
-        </label>
-        <input
-          className={styles.Input}
-          type="text"
-          name="place"
-          id="place"
-          onChange={newValueHandler}
-          required
-          value={inputState.place}
-        ></input>
-      </div>
-      <div className={styles.Option}>
-        <label className={styles.Label} htmlFor="tags">
-          Tagi
-        </label>
-        <input
-          className={styles.Input}
-          type="text"
-          name="tags"
-          id="tags"
-          required
-          onChange={newValueHandler}
-          value={inputState.tags}
-        ></input>
-      </div>
-    </form>
+          <Link to="/">
+            <button className={styles.Button}>Anuluj</button>
+          </Link>
+          <div className={styles.Option}>
+            <label className={styles.Label} htmlFor="name">
+              Nazwa
+            </label>
+            <input
+              className={styles.Input}
+              type="text"
+              name="name"
+              id="name"
+              value={inputState.name}
+              onChange={newValueHandler}
+              required
+            ></input>
+          </div>
+          <div className={styles.Option}>
+            <label className={styles.Label} htmlFor="url">
+              URL
+            </label>
+            <input
+              className={styles.Input}
+              type="text"
+              name="url"
+              id="url"
+              onChange={newValueHandler}
+              required
+              value={inputState.url}
+            ></input>
+          </div>
+          <div className={styles.Option}>
+            <label className={styles.Label} htmlFor="date">
+              Data
+            </label>
+            <input
+              className={styles.Input}
+              type="date"
+              name="date"
+              id="date"
+              placeholder=""
+              required
+              onChange={newValueHandler}
+              value={inputState.date}
+            ></input>
+          </div>
+          <div className={styles.Option}>
+            <label className={styles.Label} htmlFor="place">
+              Miejsce
+            </label>
+            <input
+              className={styles.Input}
+              type="text"
+              name="place"
+              id="place"
+              onChange={newValueHandler}
+              required
+              value={inputState.place}
+            ></input>
+          </div>
+          <div className={styles.Option}>
+            <label className={styles.Label} htmlFor="tags">
+              Tagi
+            </label>
+            <input
+              className={styles.Input}
+              type="text"
+              name="tags"
+              id="tags"
+              required
+              onChange={newValueHandler}
+              value={inputState.tags}
+            ></input>
+          </div>
+        </form>
   );
 };
 
