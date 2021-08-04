2d1bdc53d3dcb184609a6637467ea64af7c990f7
Klaudia <kklaudiaw@gmail.com>
2021-08-04 12:52:07 +0200

    new function: showing only favorite photos

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -8,8 +8,9 @@ import heart from '../../assets/svg/heart-solid.svg';
 import heartRed from '../../assets/svg/heart-solid-red.svg';
 
 
-const Photos = () => {
+const Photos = (props) => {
   const { files } = useFile()
+
   const handleFavorite = (item) => {
     item.favorite ? database.files.doc(item.id).update({
       favorite: false
@@ -17,20 +18,30 @@ const Photos = () => {
       favorite: true
     })
   }
-  return (
-    files.map((item) => {
-      return (
-        <article className={styles.Article} key={item.id}>
-          <div className={styles.square}><img className={styles.photo} src={item.url} alt={item.name} /></div>
-          <p className={styles.SingleLine}><span className={styles.info}>Name: </span>{item.name}</p>
-          <p className={styles.SingleLine}><span className={styles.info}>Place: </span>{item.place}</p>
-          <p className={styles.SingleLine}><span className={styles.info}>Date: </span>{item.date}</p>
-          <p className={styles.SingleLine}><span className={styles.info}>Tags: </span>{item.tags}</p>
-          <img src={item.favorite ? heartRed : heart} className={styles.Heart} alt="heart" onClick={() => handleFavorite(item)} />
-        </article>
-      );
-    })
-  );
+
+  const Article = ({ item }) => {
+    return (
+      <article className={styles.Article} key={item.id}>
+        <div className={styles.square}><img className={styles.photo} src={item.url} alt={item.name} /></div>
+        <p className={styles.SingleLine}><span className={styles.info}>Name: </span>{item.name}</p>
+        <p className={styles.SingleLine}><span className={styles.info}>Place: </span>{item.place}</p>
+        <p className={styles.SingleLine}><span className={styles.info}>Date: </span>{item.date}</p>
+        <p className={styles.SingleLine}><span className={styles.info}>Tags: </span>{item.tags}</p>
+        <img src={item.favorite ? heartRed : heart} className={styles.Heart} alt="heart" onClick={() => handleFavorite(item)} />
+      </article>
+    )
+  }
+
+  if (props.onlyFavorite) {
+    return (
+      files.filter(item => item.favorite === true)
+        .map(item => <Article item={item} />)
+    );
+  } else {
+    return (
+      files.map((item) => <Article item={item} />)
+    )
+  }
 };
 
 export default Photos;
