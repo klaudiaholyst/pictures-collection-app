343bbd27f84515fe144b02fde047cce3019f1152
Klaudia <kklaudiaw@gmail.com>
2021-08-04 11:12:15 +0200

    new function: mark picture as favorite

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -1,11 +1,22 @@
-import {useFile} from '../../hooks/useFile'
+import { useFile } from '../../hooks/useFile';
+
+import { database } from '../../firebase';
 
 import styles from './Photos.module.css';
 
 import heart from '../../assets/svg/heart-solid.svg';
+import heartRed from '../../assets/svg/heart-solid-red.svg';
+
 
 const Photos = () => {
-  const {files} = useFile()
+  const { files } = useFile()
+  const handleFavorite = (item) => {
+    item.favorite ? database.files.doc(item.id).update({
+      favorite: false
+    }) : database.files.doc(item.id).update({
+      favorite: true
+    })
+  }
   return (
     files.map((item) => {
       return (
@@ -15,7 +26,7 @@ const Photos = () => {
           <p className={styles.SingleLine}><span className={styles.info}>Place: </span>{item.place}</p>
           <p className={styles.SingleLine}><span className={styles.info}>Date: </span>{item.date}</p>
           <p className={styles.SingleLine}><span className={styles.info}>Tags: </span>{item.tags}</p>
-          <img src={heart} className={styles.Heart} alt="heart" />
+          <img src={item.favorite ? heartRed : heart} className={styles.Heart} alt="heart" onClick={() => handleFavorite(item)} />
         </article>
       );
     })
